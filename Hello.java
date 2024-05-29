1b335dd4b1e7f90c6b4c5c059cfff8f10f9116d2
seulk <skkang1@naver.com>
2024-05-29 11:29:28 +0900

    Fix_0529_1

--- Hello.java
+++ Hello.java
@@ -2,11 +2,15 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        int a = 100;
+        int a = 111;
+        // ctrl + shif + p 명령 팔레트
+        // -> Format Document
+        // -> Language Support for Java by redhat - 정렬하는 거
 
-        if (a>100) {
+        // Format on save 체크 시 저장할 때 자동정렬
+
+        if (a > 100) {
             System.out.println("하하하");
         }
     }
-    
 }
